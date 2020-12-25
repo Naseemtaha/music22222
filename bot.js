c81608b339f28d8d663b880652073f1f86266310
Naseemtaha <46400979+Naseemtaha@users.noreply.github.com>
2020-12-25 17:41:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log("Welcome Again !");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
