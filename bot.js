31964b09562b92006fbf7ee7a56aedebcd6ef1c6
Naseemtaha <46400979+Naseemtaha@users.noreply.github.com>
2020-12-25 16:31:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,23 @@
-onst Discord = require('discord.js');
-const client = new Discord.Client();
 
+cconst Discord = require("discord.js");
+const client = new Discord.Client();
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(` By The Geniuses .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
+client.on('message', message => {
+     if (message.content === ".servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+
+
+
+
+
 
 
 
